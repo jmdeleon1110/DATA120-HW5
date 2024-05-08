48b75d411c27a50a78f6b0ae4d1ebd9075ead207
jmdeleon1110 <165223582+jmdeleon1110@users.noreply.github.com>
2024-05-07 23:00:34 -0500

    Update pa5.py

--- pa5.py
+++ pa5.py
@@ -3,26 +3,25 @@
 
 # 0.0.1 Problem 1: GCD
 
-# In[1]:
 def gcd(a, b):
     if b == 0:
         return a
-    if a ==b:
+    if a == b:
         return a
     if a < b:
         return gcd(a, b % a)
     if b < a:
         return gcd(b, a % b)
 
+
 # 0.0.2 Problem 2: Directions
 
-# In[40]:
 def remove_pairs(directions):
     turnarounds = {'EW', 'WE', 'NS', 'SN'}
 
     if len(directions) <= 1:
         return directions
-    
+
     pair = directions[:2]
     if pair in turnarounds:
         return remove_pairs(directions[2:])
@@ -30,21 +29,10 @@ def remove_pairs(directions):
         return directions[0] + remove_pairs(directions[1:])
 
 
-# In[39]:
-
-remove_pairs('EEWN')
-
-# In[41]:
-
-remove_pairs('SSNS')
-
-# In[42]:
-
-remove_pairs("ESNW")
-
 # 0.0.3 Problem 3: Bisection Method
 
-# In[56]:
+import math
+
 def bisection_root(equation, x1, x2):
     if equation(x1) * equation(x2) > 0:
         raise ValueError('Cannot find the root between these values')
@@ -60,19 +48,3 @@ def bisection_root(equation, x1, x2):
         return bisection_root(equation, x, x2)
     else:
         raise ValueError('Cannot find a root between these values')
-# In[54]:
-
-import math
-
-
-# In[55]:
-
-
-bisection_root(math.sin, 2, 4)
-
-
-# In[ ]:
-
-
-
-
